0446044c104b2eac2a7cc980a9f7fe7d3e5c890c
fabienbrun <fabienbrun@hotmail.fr>
2015-09-30 12:36:26 +0200

    Animation Arrow

--- dev/cside/web/front/app/directives/animArrow.js
+++ dev/cside/web/front/app/directives/animArrow.js
@@ -11,43 +11,23 @@ app.directive('animArrow', ['$log', 'DeviceDetector', function ($log, $device) {
         {
 
 
+            scope.arrowMouseOver = function(arrowId){
 
-
-            scope.arrowMouseOver = function(scope, element, attrs){
-
-                var arrow =  angular.element(document.querySelector(".arrow-white"));
+                var arrow =  angular.element(document.querySelector("." + arrowId));
                 TweenMax.to(arrow,0.3,{delay:0,x:"50%",force3D:true});
                 TweenMax.to(arrow,0.3,{delay:0.2,x:"-50%",force3D:true});
             }
 
-            scope.arrowMouseOut = function(scope, element, attrs){
+            scope.arrowMouseOut = function(arrowId){
 
-                var arrow =  angular.element(document.querySelector(".arrow-white"));
+                var arrow =  angular.element(document.querySelector("." + arrowId));
                 TweenMax.to(arrow,2,{opacity:1, force3D:true});
             }
 
 
-
-
-
-
         }
 
-        /* else if($device.tablet() != null)
-         {
-         var $deco = element[0].querySelector(".decoration");
-         var $label = element[0].querySelector(".label");
-         element.bind("touchstart",function()
-         {
-         TweenMax.to($deco,.2,{top:0});
-         TweenMax.to($label,.2,{opacity:1});
-         });
-         element.bind("touchend",function()
-         {
-         TweenMax.to($deco,.2,{top:"100%"});
-         TweenMax.to($label,.2,{opacity:0});
-         });
-         }*/
+
     }
 
 
