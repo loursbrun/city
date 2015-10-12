8effb2d85d449475d75a477d336549e98a9c9dd9
fabienbrun <fabienbrun@hotmail.fr>
2015-10-12 15:58:19 +0200

    Wording link localized

--- dev/cside/web/front/app/controllers/ViewBoxsetController.js
+++ dev/cside/web/front/app/controllers/ViewBoxsetController.js
@@ -8,7 +8,7 @@
 // @author Fabien Brun
 //
 
-app.controller("ViewBoxsetController", ['$scope', '$log', 'DeviceDetector','$state', function ViewBoxsetController($scope, $log, device, $state)
+app.controller("ViewBoxsetController", ['$scope', '$log', 'DeviceDetector','$state','AnimationModel', function ViewBoxsetController($scope, $log, device, $state, model)
 {
 
     $log.log("View Boxset Controller");
@@ -27,6 +27,13 @@ app.controller("ViewBoxsetController", ['$scope', '$log', 'DeviceDetector','$sta
     $scope.viewBoxset =  angular.element(document.querySelector(".view-boxset"));
     TweenMax.to($scope.viewBoxset,0,{opacity:0, force3D:true});
     TweenMax.to($scope.viewBoxset,0.5,{delay:0.2, opacity:1, force3D:true});
+
+
+    // Wordings Localised
+    $scope.box_set_description = model.localized.labels_dictionaries['box-set'].description ;
+    $scope.box_set_title = model.localized.labels_dictionaries['box-set'].title ;
+    $scope.back_wording = model.localized.labels_dictionaries.common.back ;
+    $scope.exceptional_case_wording = model.localized.labels_dictionaries.common.exceptional_case ;
 
 
 
--- dev/cside/web/front/app/controllers/ViewExceptionalcaseController.js
+++ dev/cside/web/front/app/controllers/ViewExceptionalcaseController.js
@@ -15,8 +15,6 @@ app.controller("ViewExceptionalcaseController", ['$scope', '$log', 'DeviceDetect
 
     device.setLayout(DeviceDetector.LAYOUT_FILL);
 
-
-
     // Sku
     $scope.book_sku = model.localized.labels_dictionaries["exceptional-case"].sku ;
 
@@ -27,13 +25,19 @@ app.controller("ViewExceptionalcaseController", ['$scope', '$log', 'DeviceDetect
 
 
 
-
     // Anim exceptionalcase view
     $scope.viewExceptionalcase =  angular.element(document.querySelector(".view-exceptionalcase"));
     TweenMax.to($scope.viewExceptionalcase,0,{opacity:0, force3D:true});
     TweenMax.to($scope.viewExceptionalcase,0.5,{delay:0.2, opacity:1, force3D:true});
 
 
+
+    // Wordings Localised
+    $scope.exceptional_case_description = model.localized.labels_dictionaries['exceptional-case'].description ;
+    $scope.exceptional_case_title = model.localized.labels_dictionaries['exceptional-case'].title ;
+    $scope.more_wording = model.localized.labels_dictionaries.common.more ;
+    $scope.back_wording = model.localized.labels_dictionaries.common.back ;
+    $scope.box_set_wording = model.localized.labels_dictionaries.common.box_set ;
 
 
 
